554537cc71cf06460daeef8d0657d0c4a9d12e98
Yoann L'hostis <yolhostis@live.fr>
2021-06-15 20:10:10 +0200

    Les 3 boutons sont fonctionnels

--- js/Appliances.js
+++ js/Appliances.js
@@ -2,9 +2,11 @@
 class Appliances {
 	constructor(appliances) {
 		this.data = appliances;
-		this.sortedData = []; // ???
-		this.containerAppliances = document.getElementById("containerAppliances");
+		this.dataSortedByInput = [];
+		this.dataSortedByMainSearch = []; // ???
 		this.containerKeywords = document.getElementById("containerKeywords");
+		this.input = document.getElementById("searchInMenuAppliances");
+		this.containerAppliances = document.getElementById("containerAppliances");
 	}
 	displayAppliances() {
 		let i = 0;
@@ -13,18 +15,7 @@ class Appliances {
 				i++;
 				return '<p id="appliance' + i + '">' + appliance + '</p>';
 			}).join('');
-	}
-
-	// displaySortedAppliances(sortedData) { // ??? dans l'idée de l'envoi d'un tableau trié dans les algos
-	// 	console.log(sortedData);
-	// 	return this.containerAppliances.innerHTML = 
-	// 		this.sortedData.map(appliance => {
-	// 			return '<p>' + appliance + '</p>';
-	// 		}).join('');
-	// }
-	
-	sortKeywords() {
-		
+		this.clickAndDisplayKeyword();
 	}
 	clickAndDisplayKeyword() {
 		for(let i = 1; i <= this.data.length; i++) {
@@ -38,6 +29,88 @@ class Appliances {
 				this.closeKeyword();
 			});
 		}	
+	}
+	// Rectification: les mots restants dépendent apparemment de la frappe dans l'input...
+	// Passer tous les keywords en string (sans espace, sans de, en, d', etc pour alléger la recherche)
+	// => 'j'u's'c'i't'r'o'n' et checker occurences ???
+	listenInputAndDisplaySortedKeywords() {
+		// this.input.addEventListener("input", (event) => {
+		// 	for(const ingredient of this.data) {
+		// 		const regex1 = new RegExp(/d'/);
+		// 		const regex2 = new RegExp(/ /);
+		// 		let resultWithoutD = ingredient.replace(regex1, "");
+		// 		let result = resultWithoutD.split(regex2);
+		// 		for(const word of result) {
+		// 			if(event.target.value == word) {
+		// 				this.dataSortedByInput.push(ingredient);
+		// 				this.containerIngredients.innerHTML = 
+		// 					this.dataSortedByInput.map(ingredient => {
+		// 						return  '<p id="' + ingredient + '">' + ingredient + '</p>';
+		// 					}).join('');
+		// 			}
+		// 			if(event.target.value == "") {
+		// 				this.dataSortedByInput = [];
+		// 				this.displayIngredients();
+		// 			}
+		// 			if(event.target.value == "la") {
+		// 				return;
+		// 			}
+		// 			if(event.target.value == "en") {
+		// 				return;
+		// 			}
+		// 			if(event.target.value == "au") {
+		// 				return;
+		// 			}
+		// 			if(event.target.value == "a") {
+		// 				return;
+		// 			}
+		// 			if(event.target.value == "ou") {
+		// 				return;
+		// 			}	
+		// 		}
+		// 	}
+		// });
+		
+		this.input.addEventListener("input", (event) => {
+			this.dataSortedByInput = [];
+			for(const appliance of this.data) {
+				const regex1 = new RegExp(/d'/);
+				const regex2 = new RegExp(/ /);
+				let resultWithoutD = appliance.replace(regex1, "");
+				let result = resultWithoutD.split(regex2);
+				for(const word of result) {
+					let splitted = word.split('');
+					if(event.target.value == splitted.splice(0, event.target.value.length).join('')) {
+						this.dataSortedByInput.push(appliance);
+						this.containerAppliances.innerHTML = 
+							this.dataSortedByInput.map(appliance => {
+								return  '<p id="' + appliance + '">' + appliance + '</p>';
+							}).join('');
+					}
+				}
+			}	
+			if(event.target.value == "") {
+				this.dataSortedByInput = [];
+				this.displayAppliances();
+			}
+			this.clickAndDisplaySortedKeyword(); 
+		});
+	}
+	clickAndDisplaySortedKeyword() { 
+		console.log("clickAndDisplaySortedKeyword()");
+		for(const appliance of this.dataSortedByInput) {
+			document.getElementById(appliance).addEventListener("click", (event) => {
+				console.log("click on " + appliance);
+				selectedKeywords.push({"name": event.target.innerText, "color": "danger"});
+				this.containerKeywords.innerHTML = 
+					selectedKeywords.map(element => {
+						return '<div id="' + element.name + '" class="keyword bg-' + element.color + ' flex"><p>' + element.name + '</p><div><i class="far fa-times-circle"></i></div></div>';
+					}).join('');
+				this.displayAppliances();
+				this.input.value = "";
+				this.closeKeyword();
+			});
+		}
 	}
 	closeKeyword() {
 		console.log("closeKeyword()");
@@ -59,5 +132,12 @@ class Appliances {
 			})
 		}
 	}
+	// displaySortedAppliances(sortedData) { // ??? dans l'idée de l'envoi d'un tableau trié dans les algos
+	// 	console.log(sortedData);
+	// 	return this.containerAppliances.innerHTML = 
+	// 		this.sortedData.map(appliance => {
+	// 			return '<p>' + appliance + '</p>';
+	// 		}).join('');
+	// }
 }
 
--- js/Ustensils.js
+++ js/Ustensils.js
@@ -2,9 +2,11 @@
 class Ustensils {
 	constructor(ustensils) {
 		this.data = ustensils;
-		this.sortedData = []; // ???
-		this.containerUstensils = document.getElementById("containerUstensils");
+		this.dataSortedByInput = [];
+		this.dataSortedByMainSearch = []; // ???
 		this.containerKeywords = document.getElementById("containerKeywords");
+		this.input = document.getElementById("searchInMenuUstensils"); 
+		this.containerUstensils = document.getElementById("containerUstensils");
 	}
 	displayUstensils() {
 		let i = 0;
@@ -13,18 +15,7 @@ class Ustensils {
 				i++;
 				return '<p id="ustensil' + i + '">' + ustensil + '</p>';
 			}).join('');
-	}
-
-	// displaySortedUstensils(sortedData) { // ??? dans l'idée de l'envoi d'un tableau trié dans les algos
-	// 	console.log(sortedData);
-	// 	return this.containerUstensils.innerHTML = 
-	// 		this.sortedData.map(ustensil => {
-	// 			return '<p>' + ustensil + '</p>';
-	// 		}).join('');
-	// }
-	
-	sortKeywords() {
-		
+		this.clickAndDisplayKeyword();
 	}
 	clickAndDisplayKeyword() {
 		for(let i = 1; i <= this.data.length; i++) {
@@ -35,6 +26,88 @@ class Ustensils {
 						return '<div id="' + element.name + '" class="keyword bg-' + element.color + ' flex"><p>' + element.name + '</p><div><i class="far fa-times-circle"></i></div></div>';
 					}).join('');
 					this.closeKeyword();
+			});
+		}
+	}
+	// Rectification: les mots restants dépendent apparemment de la frappe dans l'input...
+	// Passer tous les keywords en string (sans espace, sans de, en, d', etc pour alléger la recherche)
+	// => 'j'u's'c'i't'r'o'n' et checker occurences ???
+	listenInputAndDisplaySortedKeywords() {
+		// this.input.addEventListener("input", (event) => {
+		// 	for(const ingredient of this.data) {
+		// 		const regex1 = new RegExp(/d'/);
+		// 		const regex2 = new RegExp(/ /);
+		// 		let resultWithoutD = ingredient.replace(regex1, "");
+		// 		let result = resultWithoutD.split(regex2);
+		// 		for(const word of result) {
+		// 			if(event.target.value == word) {
+		// 				this.dataSortedByInput.push(ingredient);
+		// 				this.containerIngredients.innerHTML = 
+		// 					this.dataSortedByInput.map(ingredient => {
+		// 						return  '<p id="' + ingredient + '">' + ingredient + '</p>';
+		// 					}).join('');
+		// 			}
+		// 			if(event.target.value == "") {
+		// 				this.dataSortedByInput = [];
+		// 				this.displayIngredients();
+		// 			}
+		// 			if(event.target.value == "la") {
+		// 				return;
+		// 			}
+		// 			if(event.target.value == "en") {
+		// 				return;
+		// 			}
+		// 			if(event.target.value == "au") {
+		// 				return;
+		// 			}
+		// 			if(event.target.value == "a") {
+		// 				return;
+		// 			}
+		// 			if(event.target.value == "ou") {
+		// 				return;
+		// 			}	
+		// 		}
+		// 	}
+		// });
+		
+		this.input.addEventListener("input", (event) => {
+			this.dataSortedByInput = [];
+			for(const ustensil of this.data) {
+				const regex1 = new RegExp(/d'/);
+				const regex2 = new RegExp(/ /);
+				let resultWithoutD = ustensil.replace(regex1, "");
+				let result = resultWithoutD.split(regex2);
+				for(const word of result) {
+					let splitted = word.split('');
+					if(event.target.value == splitted.splice(0, event.target.value.length).join('')) {
+						this.dataSortedByInput.push(ustensil);
+						this.containerUstensils.innerHTML = 
+							this.dataSortedByInput.map(ustensil => {
+								return  '<p id="' + ustensil + '">' + ustensil + '</p>';
+							}).join('');
+					}
+				}
+			}	
+			if(event.target.value == "") {
+				this.dataSortedByInput = [];
+				this.displayIngredients();
+			}
+			this.clickAndDisplaySortedKeyword(); 
+		});
+	}
+	clickAndDisplaySortedKeyword() { 
+		console.log("clickAndDisplaySortedKeyword()");
+		for(const ustensil of this.dataSortedByInput) {
+			document.getElementById(ustensil).addEventListener("click", (event) => {
+				console.log("click on " + ustensil);
+				selectedKeywords.push({"name": event.target.innerText, "color": "success"});
+				this.containerKeywords.innerHTML = 
+					selectedKeywords.map(element => {
+						return '<div id="' + element.name + '" class="keyword bg-' + element.color + ' flex"><p>' + element.name + '</p><div><i class="far fa-times-circle"></i></div></div>';
+					}).join('');
+				this.displayUstensils();
+				this.input.value = "";
+				this.closeKeyword();
 			});
 		}
 	}
@@ -58,6 +131,13 @@ class Ustensils {
 			})
 		}
 	}
+		// displaySortedUstensils(sortedData) { // ??? dans l'idée de l'envoi d'un tableau trié dans les algos
+	// 	console.log(sortedData);
+	// 	return this.containerUstensils.innerHTML = 
+	// 		this.sortedData.map(ustensil => {
+	// 			return '<p>' + ustensil + '</p>';
+	// 		}).join('');
+	// }
 }
 
 
