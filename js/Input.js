5201f0986322b4315bb7b3641de2f142503c7757
Yoann L'hostis <yolhostis@live.fr>
2021-07-26 17:35:01 +0200

    Modification algo 2

--- js/Input.js
+++ js/Input.js
@@ -15,9 +15,6 @@ class Input {
 				console.time('input.listeningChanges()');
 				this.cards = [];
 				this.sortAllData();
-				// this.sortIngredients();
-				// this.sortDescriptions();
-				//this.deleteDoubles();
 				console.timeEnd('input.listeningChanges()');
 			}
 			if(event.target.value.length >= 3 && this.cards.length == 0) {
@@ -25,6 +22,23 @@ class Input {
 					'<p style="margin: auto; color: red">Aucune recette ne correspond à votre critère... vous pouvez chercher « tarte aux pommes », « poisson », etc';
 			}
 		})
+	}
+	createAndPushCard(recipe) {
+		const card = new Card(
+			recipe.id,
+			recipe.name,
+			recipe.ingredients,
+			recipe.time,
+			recipe.description,
+			recipe.appliance,
+			recipe.ustensils);
+		this.cards.push(card);
+	}
+	displaySortedCards() {
+		this.containerCards.innerHTML = 
+			this.cards.map(card => {
+				return card.render();
+			}).join('');
 	}
 	sortAllData() {
 		const recipes = [];
@@ -34,61 +48,28 @@ class Input {
 		for(const recipe of recipes) {
 			for(let i = 0; i <= recipe.name.length; i++) { 
 				if(event.target.value == recipe.name.toLowerCase().split('').splice(i, event.target.value.length).join('')) {
-					const card = new Card(
-						recipe.id,
-						recipe.name,
-						recipe.ingredients,
-						recipe.time,
-						recipe.description,
-						recipe.appliance,
-						recipe.ustensils);
-					this.cards.push(card);
+					this.createAndPushCard(recipe);	
 				}
 			}
-			this.containerCards.innerHTML = 
-			this.cards.map(card => {
-				return card.render();
-			}).join('');	
+			this.displaySortedCards();
 		}
 		for(const recipe of recipes) {
 			for(const ingredient of recipe.ingredients) {
 				for(let i = 0; i <= ingredient.ingredient.length; i++) { 
 					if(event.target.value == ingredient.ingredient.toLowerCase().split('').splice(i, event.target.value.length).join('')) {
-						const card = new Card(
-							recipe.id,
-							recipe.name,
-							recipe.ingredients,
-							recipe.time,
-							recipe.description,
-							recipe.appliance,
-							recipe.ustensils);
-						this.cards.push(card);
+						this.createAndPushCard(recipe);
 					}
 				}
 			}
-			this.containerCards.innerHTML = 
-			this.cards.map(card => {
-				return card.render();
-			}).join('');
+			this.displaySortedCards();
 		}
 		for(const recipe of recipes) {
 			for(let i = 0; i <= recipe.description.length; i++) { 
 				if(event.target.value == recipe.description.toLowerCase().split('').splice(i, event.target.value.length).join('')) {
-					const card = new Card(
-						recipe.id,
-						recipe.name,
-						recipe.ingredients,
-						recipe.time,
-						recipe.description,
-						recipe.appliance,
-						recipe.ustensils);
-					this.cards.push(card);
+					this.createAndPushCard(recipe);
 				}
 			}
-			this.containerCards.innerHTML = 
-			this.cards.map(card => {
-				return card.render();
-			}).join('');
+			this.displaySortedCards();
 		}
 	}
 	clickOnSearch() {
