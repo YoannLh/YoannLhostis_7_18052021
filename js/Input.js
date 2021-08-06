a2266fd6e5b2272a5cf12c158887bcd8291f000f
Yoann L'hostis <yolhostis@live.fr>
2021-08-06 02:09:14 +0200

    Approche BMH

--- js/Input.js
+++ js/Input.js
@@ -59,7 +59,7 @@ class Input {
 		}
 		for(const recipe of recipes) {
 			for(const ingredient of recipe.ingredients) {
-				for(let i = 0; i <= ingredient.ingredient.length; i++) { 
+				for(let i = 0; i <= ingredient.ingredient.length; i++) {
 					if(event.target.value == ingredient.ingredient.toLowerCase().split('').splice(i, event.target.value.length).join('')) {
 						this.createAndPushCard(recipe);
 					}
@@ -86,3 +86,63 @@ class Input {
 const input = new Input();
 input.listeningChanges();
 input.clickOnSearch();
+
+// let t = "Bonjour Josiane comment allez vous ?"
+
+// let s = "comment"
+
+// const badCharTable = function(word) {
+// 	const table = {}
+// 	for(let i = 0; i < word.length - 1; i++) {
+// 		table[word[i]] = word.length - i - 1
+// 	}
+// 	return table
+// }
+
+// const moore = function(needle, haystack) {
+// 	const table = badCharTable(needle)
+// 	let skip = 0
+// 	while(haystack.length - skip >= needle.length) {
+// 		let i = needle.length - 1
+// 		while(haystack[skip + i] == needle[i]) {
+// 			if(i == 0) {
+// 				return skip
+// 			}
+// 			i--
+// 		}
+// 		skip += (table[haystack[skip + needle.length - 1]] || needle.length)
+// 	}
+// 	return -1
+// }
+
+// console.time('moore')
+// console.log(moore(s, t))
+// console.timeEnd('moore')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
